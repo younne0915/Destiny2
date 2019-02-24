742b0dbee4eb14ebe9a76af8d1990aad3a3ac73f
younne0915 <younne@163.com>
2019-02-24 21:52:38 +0800

    retGameServerEntity

---
+++ Project/Client/MMORPG/Assets/Script/Data/NetData/Entity/RetGameServerEntity.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class RetGameServerEntity
+{
+    public int Id;
+    public int RunStatus;
+    public bool isCommend;
+    public bool isNew;
+    public string Name;
+    public string Ip;
+    public int Port;
+    public DateTime CreateTime;
+    public DateTime UpdateTime;
+}
---
+++ Project/Client/MMORPG/Assets/Script/Data/NetData/Entity/RetGameServerPageEntity.cs
@@ -0,0 +1,5 @@
+public class RetGameServerPageEntity
+{
+    public int PageIndex;
+    public string Name;
+}
